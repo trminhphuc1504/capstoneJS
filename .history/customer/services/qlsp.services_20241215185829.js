4feae7d029efe2935df0ee0133b9de27128cf8b6
trminhphuc1504 <trminhphuc1504@gmail.com>
2024-12-15 19:42:56 +0700

    first

---
+++ .history/customer/services/qlsp.services_20241215185829.js
@@ -0,0 +1,10 @@
+const baseUrl = 'https://6728d9246d5fa4901b6b3112.mockapi.io/CapstoneJS'
+
+export const qlspServices = {
+    getProductList: () =>{
+        return axios({
+            method: 'GET',
+            url
+        })
+    }
+}
